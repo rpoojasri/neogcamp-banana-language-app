426c016110c9bf07cc76c791c4a87570a1c27400
R Pooja Sri <74866044+rpoojasri@users.noreply.github.com>
2020-12-07 15:14:29 +0530

    content added

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ var translateBtn = document.querySelector("#Translate-btn");
 var inputTxt = document.querySelector("#input-txt");
 var outputTxt = document.querySelector("#output")
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
+function getUrl(text)
+{
+    return serverUrl + "?" + "text=" + text
+}
 
-translateBtn.addEventListener("click", translateFn)
+function errorHandler(error){
+    console.log("Error occured:",error);
+    alert("Server is busy,try again later")
+}
+
 
 function translateFn() {
-    outputTxt.innerText = inputTxt.value;
+
+    fetch(getUrl(inputTxt.value))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated
+        outputTxt.innerText = translatedText
+    })
+    .catch(errorHandler)
 };
 
+translateBtn.addEventListener("click", translateFn)
